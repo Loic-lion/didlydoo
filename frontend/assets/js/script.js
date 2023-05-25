56cdcd336326b35f6b6832ca69e170938c1b972e
Loic Lion <loic.lion.contact@gmail.com>
2023-05-25 15:43:00 +0200

    ajout début js

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -13,25 +13,50 @@ fetch(url, options)
       let descriptionEvent = document.createElement("div");
       descriptionEvent.textContent = descriptionApi;
       let sectionEvent = document.createElement("section");
-    //   sectionEvent.classList.add("");     A COMPLETER 
+      //   sectionEvent.classList.add("");     A COMPLETER SI BESOIN D UNE CLASS POUR LES SECTIONS
       const main = document.querySelector("main");
 
-      /////////////////// Afficher le nom de l'event///////
+      /////////////////// Afficher le nom de l'event + auteur///////
 
       let nameApi = element.name;
+      let authorApi = element.author;
       let nameEvent = document.createElement("div");
-      nameEvent.textContent = nameApi;
+      nameEvent.textContent = authorApi + "'s " + nameApi;
       sectionEvent.appendChild(nameEvent);
-
-      ////////////////////////création du tableau////
-
-      let table = document.createElement("table");
 
       /////////////attaher la section au main/////
 
       sectionEvent.appendChild(descriptionEvent);
       main.appendChild(sectionEvent);
 
+      ////////////////////////création du tableau////
+
+      let table = document.createElement("table");
+      let tr = document.createElement("tr");
+      table.appendChild(tr);
+      sectionEvent.appendChild(table);
+      let trvoid = document.createElement("th");
+      tr.appendChild(trvoid);
+
+      ////////STOCKAGE DES INFOS DANS LE TABLEAU//////
+      let infosTable = element.dates;
+
+      infosTable.forEach((dates) => {
+        console.log(dates);
+        let datesEvent = document.createElement("th");
+        datesEvent.textContent = dates.date;
+        tr.appendChild(datesEvent);
+
+        // let choicesPeople = dates.attendees;
+
+        // choicesPeople.forEach((people) => {
+
+        //   let namePeople = people.name;
+
+        //   console.log(namePeople);
+
+        // });
+      });
     });
   })
   .catch((err) => {
